e55871f17fd606f94f7dffea75222e879b9549bc
kruti215 <158009754+kruti215@users.noreply.github.com>
2025-09-25 07:47:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 // Serve frontend files
 app.use(express.static(path.join(__dirname, "../frontend")));
